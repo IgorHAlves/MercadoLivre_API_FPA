907d21c63289b1e42dbd16e28f5df7e0fd1f2cb3
auauauau <auauauau@gmail.com>
2025-05-12 22:23:17 -0300

    Modelo de frontend iniciado

---
+++ Frontend-MercadoLivre/src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { ref } from "vue";
+
+import Navbar from "./components/NavbarComponent.vue";
+</script>
+
+<template>
+  <Navbar />
+  <router-view> </router-view>
+</template>
---
+++ Frontend-MercadoLivre/src/views/AboutView.vue
@@ -0,0 +1,12 @@
+<script setup>
+import NavbarComponent from "../components/NavbarComponent.vue";
+</script>
+
+<template>
+  <main>
+    <div class="about">
+      <h1>This is an about page</h1>
+    </div>
+    <NavbarComponent />
+  </main>
+</template>
